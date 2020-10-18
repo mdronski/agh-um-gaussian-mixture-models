7e0c69fd11f39674aa9557dabb490e6bc2de75df
Michał Droński <dronskimichal@gmail.com>
2020-10-18 22:20:51 +0200

    Add visualization for clustered dataset

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import numpy as np
+import seaborn as sns
 from sklearn.decomposition import PCA
 
 from src.config import DATASET_PATH
@@ -11,23 +13,32 @@ def load(dataset_name: str) -> pd.DataFrame:
                      sep='[ ]+',
                      engine='python',
                      header=None)
+    df.columns = df.columns.astype(str)
     return df
 
 
-def visualize(dataset: pd.DataFrame):
-    if len(dataset.columns) == 2:
-        visualize_2d(dataset)
+def visualize(dataset: pd.DataFrame, clustering_results: np.ndarray = None):
+    df = dataset.copy()
+    if len(dataset.columns) != 2:
+        df = reduce_dims(dataset)
+    x = df.columns[0]
+    y = df.columns[1]
+
+    if clustering_results is not None:
+        df['pred'] = clustering_results
+        palette = sns.color_palette("Paired", len(np.unique(clustering_results)))
+        sns.scatterplot(data=df, x=x, y=y,
+                        edgecolor="none",
+                        legend=False,
+                        palette=palette,
+                        hue='pred')
     else:
-        visualize_ndim(dataset)
+        sns.scatterplot(data=df, x=x, y=y, edgecolor="none")
 
 
-def visualize_2d(dataset: pd.DataFrame):
-    x = dataset.columns[0]
-    y = dataset.columns[1]
-    dataset.plot.scatter(x=x, y=y, marker='.')
-
-
-def visualize_ndim(dataset: pd.DataFrame):
+def reduce_dims(dataset: pd.DataFrame):
     pca = PCA(n_components=2)
     dataset_2d = pca.fit_transform(dataset)
-    visualize_2d(pd.DataFrame(dataset_2d))
+    df = pd.DataFrame(dataset_2d)
+    df.columns = df.columns.astype(str)
+    return df
